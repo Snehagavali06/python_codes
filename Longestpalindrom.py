7b5c0340b5fbf7b003da8e39b07caf168c4df636
Sneha Gavali <gavalisneha553@gmail.com>
2026-07-10 15:03:45 +0530

    Add longest palindromic substring finder
    Implement a function to find the longest palindromic substring in a given string.

---
+++ Longestpalindrom.py
@@ -0,0 +1,12 @@
+s = input("Enter a string: ")
+
+longest = ""
+
+for i in range(len(s)):
+    for j in range(i, len(s)):
+        sub = s[i:j+1]
+
+        if sub == sub[::-1] and len(sub) > len(longest):
+            longest = sub
+
+print("Longest Palindromic Substring:", longest)
